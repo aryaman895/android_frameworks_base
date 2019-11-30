6b7e7c43d9462fe10ffd90f7da4baa57bb1a935b
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-11-30 13:41:05 +0100

    Battery Styles: Bring back good ol' circle battery style
    * This code is based on the old Java implementation and converted to Kotlin
    
    * Instead of creating different style within the same drawable
      create one on its own, this makes the code more readable and future proof
    
    Anushek Prasal <anushekprasal@gmail.com>
    - Convert to SettingsObserver
    - Cleanup tuner code
    
    CarbonROM:
    - Remove broken battery entry from tuner list
    - Add nullcheck for percentage
    
    Dirty Unicorns:
    - Adapt 'updateShowPercent' method to existing modifications
    - Clean up code
    
    Change-Id: I23798426e603b277408fb63f4898b385027e7eeb
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4871,7 +4871,7 @@ public final class Settings {
          *    1 - Show percentage
          * @hide
          */
-        public static final String SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
+        public static final String SHOW_BATTERY_PERCENT = "dummy_show_battery_percent";
 
         /**
          * The time in ms to keep the button backlight on after pressing a button.
@@ -5894,6 +5894,21 @@ public final class Settings {
         public static final String QS_SHOW_BATTERY_PERCENT = "qs_header_show_battery_percent";
 
         /**
+         * Battery style
+         * @hide
+         */
+        public static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
+
+        /**
+          * Statusbar Battery %
+          * 0: Hide the battery percentage
+          * 1: Display the battery percentage inside the icon
+          * 2: Display the battery percentage next to Icon
+          * @hide
+          */
+        public static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
+
+        /**
          * Whether to enable the pixel navbar animation
          * @hide
          */
@@ -6017,7 +6032,6 @@ public final class Settings {
             PRIVATE_SETTINGS.add(POINTER_SPEED);
             PRIVATE_SETTINGS.add(LOCK_TO_APP_ENABLED);
             PRIVATE_SETTINGS.add(EGG_MODE);
-            PRIVATE_SETTINGS.add(SHOW_BATTERY_PERCENT);
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_MODE);
             PRIVATE_SETTINGS.add(STATUS_BAR_CLOCK);
             PRIVATE_SETTINGS.add(STATUSBAR_CLOCK_STYLE);
